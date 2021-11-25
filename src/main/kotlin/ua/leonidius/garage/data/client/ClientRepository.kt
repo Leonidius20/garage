f053f24ca3322e92b08b47f7d816e4c3d2cc7585
Leonidius20 <25536411+Leonidius20@users.noreply.github.com>
2021-11-25 17:12:18 +0200

    added repository annotation

--- src/main/kotlin/ua/leonidius/garage/data/client/ClientRepository.kt
+++ src/main/kotlin/ua/leonidius/garage/data/client/ClientRepository.kt
@@ -1,8 +1,9 @@
 package ua.leonidius.garage.data.client
 
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface ClientRepository: JpaRepository<Client, Long> {
 
 
