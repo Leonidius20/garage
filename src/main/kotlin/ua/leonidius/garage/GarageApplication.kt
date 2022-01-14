fc7f43f3bb1f7b15052f7f486f13942732fbffb1
Leonidius20 <25536411+Leonidius20@users.noreply.github.com>
2022-01-14 15:26:55 +0200

    finished periodic cache clears

--- src/main/kotlin/ua/leonidius/garage/GarageApplication.kt
+++ src/main/kotlin/ua/leonidius/garage/GarageApplication.kt
@@ -4,24 +4,41 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.scheduling.annotation.EnableAsync
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import org.springframework.web.filter.CorsFilter
 import ua.leonidius.garage.presentation.results.CarDetailReturnResult
 import java.time.LocalDate
+import java.time.LocalDateTime
 import java.util.*
 import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.TimeUnit
 
 
 @EnableAsync
+@EnableScheduling
 @SpringBootApplication
 class GarageApplication {
 
 	companion object {
-		// add date
 		val cache = ConcurrentHashMap<String, Pair<LocalDate, CarDetailReturnResult>>()
 		val pageCache = ConcurrentHashMap<Int, Pair<LocalDate, Collection<CarDetailReturnResult>>>()
 		val searchCache = ConcurrentHashMap<String, Pair<LocalDate, Collection<CarDetailReturnResult>>>()
+	}
+
+	@Scheduled(fixedDelay = 86400000, initialDelay = 0) // fixedDelay = 24h
+	fun clearOldCache() {
+		cache.clear()
+		pageCache.clear()
+		searchCache.clear()
+	}
+
+	@Scheduled(fixedDelay = 3600000, initialDelay = 3600000) // every hour
+	fun clearLargeCache() {
+		if (cache.size > 10000) cache.clear()
+		if (pageCache.size > 10000 / 15) pageCache.clear() // 15 items per page
 	}
 
 	@Bean
